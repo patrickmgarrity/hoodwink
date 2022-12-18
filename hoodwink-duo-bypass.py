49132d0ecc9ee0d72c70f8d5f754083aad226e90
patrickmgarrity <patrickmgarrity@gmail.com>
2022-12-18 07:36:34 -0500

    Create hoodwink-duo-bypass.py

---
+++ hoodwink-duo-bypass.py
@@ -0,0 +1,18 @@
+import json
+
+# Load the JSON file
+with open("insert-your-log-filename.json", "r") as f:
+  data = json.load(f)
+
+# Initialize an empty set to store the unique usernames
+unique_usernames = set()
+
+# Iterate through the data
+for item in data["data"]:
+  # Check if the result is successful and if the reason is either "Bypass Status" or "Bypass Code"
+  if item["Result"] == "SUCCESS" and (item["Factor"] == "Bypass Status" or item["Reason"] == "Bypass Code"):
+    # Add the username to the set of unique usernames
+    unique_usernames.add(item["User"])
+
+# Convert the set of unique usernames to a list and print it
+print(list(unique_usernames))
